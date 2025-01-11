b026faa42e91151830e8ad66cf5390b06abba501
Jim Roskind <jar_git@roskind.com>
2025-01-10 20:52:54 -0800

    Support alternate Shakespeare source text file

--- data/shakespeare_char/prepare.py
+++ data/shakespeare_char/prepare.py
@@ -11,6 +11,7 @@ import numpy as np
 
 # download the tiny shakespeare dataset
 input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
+#input_file_path = os.path.join(os.path.dirname(__file__), 'cut-down-t8.shakespeare.txt')
 if not os.path.exists(input_file_path):
     data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
     with open(input_file_path, 'w') as f:
